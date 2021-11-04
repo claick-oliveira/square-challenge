f03e22b7a08f229fcd54968e2b09f7dbbec57d89
Wesley Rodrigues <wesley.it@gmail.com>
2021-11-04 16:52:42 -0300

    Added ruby version

---
+++ square.rb
@@ -0,0 +1,16 @@
+def sqrt(number)
+    counter = 2
+    while counter < number
+        if counter * counter == number
+            return counter
+        end
+        counter = counter + 1;
+    end
+    return 0;
+end
+
+
+puts("The root of 25 is #{sqrt(25)}")
+puts("The root of 100 is #{sqrt(100)}")
+puts("The root of 1000000 is #{sqrt(1000000)}")
+puts("The root of 1000000000000000000 is #{sqrt(1000000000000000000)}")
